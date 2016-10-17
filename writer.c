973ef47fa3be19d0239d2afe52874b73c3a894d4
Michael Brand <miketbrand@users.noreply.github.com>
2016-10-17 16:50:31 -0500

    Create writer.c

---
+++ writer.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+        char buffer[4];
+        FILE *ptr_myfile;
+        char packet[] = "Hello World! I am your new overlord! These are random! Thanks to the changes that I just made, I am now able to send much larger packets.";
+        int packetIndex = 0;
+        int count = 1;
+
+        const char unsigned pulse257[4] = {0x01, 0x01, 0x00, 0x00};
+        const char unsigned pulse771[4] = {0x03, 0x03, 0x00, 0x00};
+        const char unsigned space257[4] = {0x01, 0x01, 0x00, 0x00};
+        const char unsigned flag[4] = {0x01, 0x10, 0x00, 0x00};
+
+        int packetSize = sizeof(packet) - 1;
+        printf("Packet size = %d\n",packetSize);
+
+        while(packetIndex < packetSize) {
+
+                int subSize = 63;
+                if (packetSize - packetIndex < subSize) {       subSize = packetSize - packetIndex;     }
+                char subPacket[63];
+                memcpy(subPacket,&packet[packetIndex],subSize);
+
+                ptr_myfile=fopen("/dev/lirc0","wb");
+                if (!ptr_myfile)
+                {
+                        printf("Unable to open file!\n");
+                        return 1;
+                }
+                else
+                {
+                        printf("Was able to open the file.\n");
+                }
+
+                // write the terminal flag
+                size_t ret = fwrite(flag,1,4,ptr_myfile);
+                printf("count = %d : ret = %d\n",count++,(int)((ssize_t)ret));
+
+                // write a space
+                ret = fwrite(space257,1,4,ptr_myfile);
+                printf("count = %d : ret = %d\n",count++,(int)((ssize_t)ret));
+
+                int i;
+                for (i=0; i<subSize; i++) {
+                        int charInt = subPacket[i];
+                        int j;
+                        for (j=0; j<8; j++) {
+                                if (charInt >= (1<<(7-j))) {
+                                        // write a pulse 1
+                                        ret = fwrite(pulse771,1,4,ptr_myfile);
+                                        printf("count = %d : ret = %d\n",count++,(int)((ssize_t)ret));
+                                        charInt = charInt - (1<<(7-j));
+                                } else {
+                                        // write a pulse 0
+                                        ret = fwrite(pulse257,1,4,ptr_myfile);
+                                        printf("count = %d : ret = %d\n",count++,(int)((ssize_t)ret));
+                                }
+
+                                // write a space
+                                ret = fwrite(space257,1,4,ptr_myfile);
+                                printf("count = %d : ret = %d\n",count++,(int)((ssize_t)ret));
+                        }
+                }
+
+                // write another terminal flag
+                ret = fwrite(flag,1,4,ptr_myfile);
+                printf("count = %d : ret = %d\n",count++,(int)((ssize_t)ret));
+
+                // write a space
+//                ret = fwrite(space257,1,4,ptr_myfile);
+//                printf("count = %d : ret = %d\n",count++,(int)((ssize_t)ret));
+
+                printf("End of file\n");
+
+                fclose(ptr_myfile);
+                packetIndex = packetIndex + subSize;
+        }
+        return 0;
+}
