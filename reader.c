f64d73e4810c59a18d92b1714d8275bff3a011a4
Michael Brand <miketbrand@users.noreply.github.com>
2016-10-17 19:02:38 -0500

    Update reader.c

--- reader.c
+++ reader.c
@@ -10,30 +10,32 @@ int main()
 
         ptr_myfile=fopen("/dev/lirc0","rb");
         if (!ptr_myfile)
-        {       
+        {
                 printf("Unable to open file!\n");
                 return 1;
         }
-        else
-        {       
-                printf("Was able to open the file.\n");
-        }
+
 
         int byteIndex = 0;
         int byteInt = 0;
         while (fread(buffer, 1, 4, ptr_myfile) == 4) {
-                if((((int)buffer[1]) < 4) && (((int)buffer[2]) == 0))
-                {       
-                        if((int)buffer[3])
-                        {       
-                                long pulseLength = 256*(256*((int)buffer[2])+((int)buffer[1]))+buffer[0];
+                long pulseLength = 256*(256*((int)buffer[2])+((int)buffer[1]))+((int)buffer[0]);
+                if((int)buffer[3]) {
+                        if (pulseLength > 3000) {
+                                packet[packetIndex] = '\0';
+                                printf("%s\n",packet);
+                                memset(packet,0,sizeof(packet));
+                                packetIndex = 0;
+                        }
+                        else
+                        {
                                 byteInt*=2;
-                                if(pulseLength > 500)
-                                {       
+                                if (pulseLength > 500)
+                                {
                                         byteInt++;
                                 }
-                              if(byteIndex == 7)
-                                {       
+                                if (byteIndex == 7)
+                                {
                                         packet[packetIndex] = byteInt;
                                         packetIndex++;
                                         byteIndex = 0;
@@ -47,10 +49,13 @@ int main()
                 }
                 else
                 {
-                        packet[packetIndex] = '\0';
-                        printf("%s\n",packet);
-                        memset(packet,0,sizeof(packet));
-                        packetIndex = 0;
+                        if (pulseLength > 100000) {
+                                printf("Timeout: reset\n");
+                                byteInt = 0;
+                                byteIndex = 0;
+                                memset(packet,0,sizeof(packet));
+                                packetIndex = 0;
+                        }
                 }
         }
 
