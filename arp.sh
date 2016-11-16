af2c73f2e8c7002ccab37a9b0010d9effc5eb221
Mitchell Bertsch <github@mitchbertsch.net>
2016-11-16 15:50:29 -0600

    Updated shell scripts

--- arp.sh
+++ arp.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 #tap0 IP MAC
-echo $1
-echo $2
-echo $3
 arp -i $1 -s $2 $3
--- tap.sh
+++ tap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 #tap0 MTU MAC IP
-echo $1
-echo $2
-echo $3
-echo $4
 ifconfig $1 down
 ifconfig $1 mtu $2
 ifconfig $1 hw ether $3
